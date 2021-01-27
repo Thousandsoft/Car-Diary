061af1da097e3d26731d9fe9f7786247b30dea8a
Thousandsoft <77388518+Thousandsoft@users.noreply.github.com>
2021-01-27 14:14:26 +0500

    Initial Commit

---
+++ Car Diary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Car Diary
+//
+//  Created by Admin on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
